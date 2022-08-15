093493b52b79ebf41e7aa190be2a61f7312c51ab
Maria Filatkina <mfilatkina97@gmail.com>
2022-08-15 16:03:38 +0300

    add lints and meta tags

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { AppProps } from "../node_modules/next/app";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Мария Филаткина - frontend-разработчик</title>
+        <meta
+          name="description"
+          content="Здесь вы можете ознакомиться с моими работами и узнать больше обо мне"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home(): JSX.Element {
+  return <div className={styles.container}>home page</div>;
+}
