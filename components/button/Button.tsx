bd0cb35dc09670844521d08324e3bbd20b45e307
Maria Filatkina <mfilatkina97@gmail.com>
2022-08-19 19:43:45 +0300

    update button props

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -3,13 +3,19 @@ import { ButtonProps } from "./Button.props";
 
 import styles from "./Button.module.scss";
 
-function Button({ appearance, children }: ButtonProps): JSX.Element {
+function Button({
+  appearance,
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element {
   return (
     <button
-      className={cn(styles.button, {
+      className={cn(styles.button, className, {
         [styles.primary]: appearance === "primary",
         [styles.ghost]: appearance === "ghost",
       })}
+      {...props}
     >
       {children}
     </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="primary" className="dsddf">
+        Кнопка
+      </Button>
       <Button appearance="ghost">Кнопка</Button>
     </>
   );
