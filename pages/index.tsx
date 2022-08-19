d86f8d637fb930581bd29518a5209c2cf03dd9f9
Maria Filatkina <mfilatkina97@gmail.com>
2022-08-19 17:12:09 +0300

    update button's css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import Htag from "../components/htag/Htag";
+import Button from "../components/button/Button";
+
 export default function Home(): JSX.Element {
-  return <div>home page</div>;
+  return (
+    <>
+      <Htag tag="h1">Текст</Htag>
+      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="ghost">Кнопка</Button>
+    </>
+  );
 }
