17fe0aa43c7c8cb8d013cee86586ac841a9c8bf1
Maria Filatkina <mfilatkina97@gmail.com>
2022-08-20 17:19:31 +0300

    create navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,24 @@
-function Navbar() {
-  return <div>Navbar</div>;
+import Link from "next/link";
+
+import styles from "./Navbar.module.scss";
+
+function Navbar(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <Link href="/">LOGO</Link>
+      <ul className={styles.list}>
+        <li className={styles["list-item"]}>
+          <Link href="/products/work">WORK</Link>
+        </li>
+        <li className={styles["list-item"]}>
+          <Link href="/products/about">ABOUT</Link>
+        </li>
+        <li className={styles["list-item"]}>
+          <Link href="/products/contact">CONTACT</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
